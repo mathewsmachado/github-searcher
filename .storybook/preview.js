04adb7034888f09643b7835499867d25427680b7
Mathews Machado <mathews.machadoamorim@gmail.com>
2021-07-30 01:08:25 -0300

    chore(devDeps): add storybook

---
+++ .storybook/preview.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
+
+export const decorators = [
+  (Story) => (
+    <>
+      <span>Revolve this with Global Contexts/Providers</span>
+      <Story />
+    </>
+  ),
+];
