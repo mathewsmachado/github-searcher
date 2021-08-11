c189aee4fad308f5010778d8951b1cfdbab33d60
Mathews Machado <mathews.machadoamorim@gmail.com>
2021-08-10 21:27:06 -0300

    test(utils): add tests to custom render method

---
+++ src/utils/tests/tests.tsx
@@ -0,0 +1,44 @@
+import styled from 'styled-components';
+
+import { render, screen } from '.';
+
+describe('customRender', () => {
+  const Title = styled.h1`
+    color: ${({ theme }) => theme.color.accent};
+  `;
+
+  it('should render the passed component', () => {
+    render(<Title>Github Searcher</Title>);
+
+    expect(screen.getByRole('heading')).toHaveTextContent(/github searcher/i);
+  });
+
+  it('should provide to the passed component access to the global providers', () => {
+    render(<Title>Github Searcher</Title>);
+
+    expect(screen.getByRole('heading')).toHaveStyle({ color: '#FF007F' });
+  });
+
+  it('should render the passed component wrapped by a div with a "data-testid" prop', () => {
+    render(<Title>Github Searcher</Title>, 'wrapper');
+    const wrapper = screen.getByTestId('wrapper');
+
+    expect(wrapper).toBeInTheDocument();
+    expect(wrapper).toMatchInlineSnapshot(`
+      .c0 {
+        color: #FF007F;
+      }
+
+      <div
+        data-testid="wrapper"
+      >
+        <h1
+          class="c0"
+        >
+          Github Searcher
+        </h1>
+      </div>
+    `);
+    expect(wrapper.firstChild).toHaveTextContent(/github searcher/i);
+  });
+});
