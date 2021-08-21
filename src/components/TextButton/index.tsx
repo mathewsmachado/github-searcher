fb1d77260189651c1075e6036a362586156f43eb
Mathews Machado <mathews.machadoamorim@gmail.com>
2021-08-21 20:07:28 -0300

    feat(components): add Text Button

---
+++ src/components/TextButton/index.tsx
@@ -0,0 +1,42 @@
+/* eslint-disable @typescript-eslint/no-use-before-define */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import styled, { css, DefaultTheme } from 'styled-components';
+
+import { Button, ButtonProps } from 'components/Button';
+
+type Props = ButtonProps & {
+  children: string;
+};
+
+// the unused vars are necessary because they're defining default values to the
+// component props
+export const TextButton = styled(Button)<Props>`
+  ${({ theme, size = 'small', color = 'accent', minimal = false }) => css`
+    width: unset;
+    height: unset;
+    font-size: ${theme.font.size.small};
+    font-weight: ${theme.font.weight.bold};
+    color: ${theme.color.secondary};
+
+    ${!!size && modifiers[size](theme)};
+  `};
+`;
+
+const modifiers = {
+  small: (theme: DefaultTheme) => css`
+    min-width: 4rem;
+    min-height: 4rem;
+    padding: 2rem ${theme.spacing.large};
+  `,
+  medium: (theme: DefaultTheme) => css`
+    min-width: 5.2rem;
+    min-height: 5.2rem;
+    padding: ${theme.spacing.small} 8rem;
+  `,
+  large: (theme: DefaultTheme) => css`
+    min-width: 6.4rem;
+    min-height: 6.4rem;
+    padding: ${theme.spacing.medium} 11rem;
+    font-size: ${theme.font.size.medium};
+  `,
+};
---
+++ src/components/TextButton/stories.tsx
@@ -0,0 +1,31 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { darkBackground, disableArgTypes } from 'utils/storybook';
+
+import { TextButton } from '.';
+
+export default {
+  title: 'TextButton',
+  component: TextButton,
+  argTypes: {
+    onClick: { action: 'clicked' },
+    color: {
+      control: { type: 'radio' },
+      options: ['primary', 'accent'],
+    },
+    ...disableArgTypes(['onClick', 'ref', 'theme', 'as', 'forwardedAs'], false),
+  },
+  ...darkBackground(),
+} as ComponentMeta<typeof TextButton>;
+
+const Template: ComponentStory<typeof TextButton> = (args) => (
+  <TextButton {...args} />
+);
+
+export const Default = Template.bind({});
+Default.args = {
+  children: 'Search User',
+  color: 'accent',
+  size: 'small',
+  minimal: false,
+};
