a29069ef42746b944aeab2659038d400455a1972
Mathews Machado <mathews.machadoamorim@gmail.com>
2021-09-22 11:56:17 -0300

    feat(core): add User mapper

---
+++ src/core/mappers/UserMapper/index.ts
@@ -0,0 +1,21 @@
+import { User, GithubUser } from 'core/entities';
+
+export class UserMapper {
+  static mapFromGithubUser(u: GithubUser): User {
+    return new User({
+      username: u.login,
+      name: u.name,
+      about: u.bio,
+      pictureUrl: u.avatar_url,
+      socialMediaUsernames: {
+        github: u.html_url,
+        twitter: u.twitter_username,
+      },
+      statuses: {
+        followers: u.followers,
+        following: u.following,
+        repositories: u.public_repos,
+      },
+    });
+  }
+}
---
+++ src/core/mappers/UserMapper/tests.ts
@@ -0,0 +1,35 @@
+import { User, GithubUser } from 'core/entities';
+
+import { UserMapper } from '.';
+
+it('should map a Github user to a User', () => {
+  const githubUser: GithubUser = {
+    name: 'Mathews Machado',
+    login: 'mathewsmachado',
+    bio: 'React developer',
+    avatar_url: './',
+    followers: 1,
+    following: 1,
+    public_repos: 1,
+    html_url: 'github.com/mathewsmachado',
+    twitter_username: 'twitter.com/mathewsmachado1',
+  };
+
+  const user = new User({
+    username: 'mathewsmachado',
+    name: 'Mathews Machado',
+    about: 'React developer',
+    pictureUrl: './',
+    socialMediaUsernames: {
+      github: 'github.com/mathewsmachado',
+      twitter: 'twitter.com/mathewsmachado1',
+    },
+    statuses: {
+      followers: 1,
+      following: 1,
+      repositories: 1,
+    },
+  });
+
+  expect(UserMapper.mapFromGithubUser(githubUser)).toStrictEqual(user);
+});
